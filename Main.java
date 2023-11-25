9de94e5f480234faf3b7a11235c9eacbf43e6784
Northern Zoro <150531424+Sever-Zoro@users.noreply.github.com>
2023-11-25 19:12:25 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,77 +2,79 @@ import java.util.Scanner;
  class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Кальулятор:");
-        System.out.println("1. Сложение");
-        System.out.println("2. Вычитание");
-        System.out.println("3. Умножение");
-        System.out.println("4. Деление");
-        System.out.println("5. Факториал");
-        System.out.println("6. Квадратный корень");
-        System.out.println("7. Возведение в тепень");
-        System.out.print("Введите вариант: ");
-        int menu = scan.nextInt();
-        if (menu == 1) {
-            System.out.print("Введите первое число: ");
-            int n1 = scan.nextInt();
-            System.out.print("Введите первое число: ");
-            int n2 = scan.nextInt();
-            int gav = Calculator.sum(n1, n2);
-            System.out.println(gav);
-        }
-        else if (menu == 2) {
-          System.out.print("Введите первое число: ");
-          int n1 = scan.nextInt();
-          System.out.print("Введите второе число: ");
-          int n2 = scan.nextInt();
-          int SSSR = Calculator.sub(n1, n2); 
-          System.out.println(SSSR);
-        }
-        else if (menu == 3) {
-        System.out.print("Введите первое число:");
-         int n1 = scan.nextInt();
-         System.out.print("Введите второе число: ");
-         int n2 = scan.nextInt();
-         int SSS = Calculator.mul(n1, n2);
-         System.out.println(SSS);
-        }
-        else if (menu == 4) { 
-            try {
-          System.out.print("Введите первое число:");
-          int n1 = scan.nextInt();
-          System.out.print("Введите второе число: ");
-          int n2 = scan.nextInt();
-          int SS = Calculator.div(n1, n2); 
-          System.out.printf("%d / %d = %d", n1, n2, SS);
-        } catch (Exception error) {
-            System.out.println(error.getMessage());
-        }
-        }
-        else if ( menu == 5) {
-          System.out.print("Введите степень факториала: ");
-          int num = scan.nextInt();
-          long res = Calculator.fact(num);
-          System.out.printf("%d! = %d", num, res);
-        }
-        else if (menu == 6) {
+        while(true){
+            System.out.println("Кальулятор:");
+            System.out.println("1. Сложение");
+            System.out.println("2. Вычитание");
+            System.out.println("3. Умножение");
+            System.out.println("4. Деление");
+            System.out.println("5. Факториал");
+            System.out.println("6. Квадратный корень");
+            System.out.println("7. Возведение в тепень");
+            System.out.print("Введите вариант: ");
+            int menu = scan.nextInt();
+            if (menu == 1) {
+                System.out.print("Введите первое число: ");
+                int n1 = scan.nextInt();
+                System.out.print("Введите первое число: ");
+                int n2 = scan.nextInt();
+                int gav = Calculator.sum(n1, n2);
+                System.out.println(gav);
+            }
+            else if (menu == 2) {
+              System.out.print("Введите первое число: ");
+              int n1 = scan.nextInt();
+              System.out.print("Введите второе число: ");
+              int n2 = scan.nextInt();
+              int SSSR = Calculator.sub(n1, n2); 
+              System.out.println(SSSR);
+            }
+            else if (menu == 3) {
+            System.out.print("Введите первое число:");
+             int n1 = scan.nextInt();
+             System.out.print("Введите второе число: ");
+             int n2 = scan.nextInt();
+             int SSS = Calculator.mul(n1, n2);
+             System.out.println(SSS);
+            }
+            else if (menu == 4) { 
+                try {
+              System.out.print("Введите первое число:");
+              int n1 = scan.nextInt();
+              System.out.print("Введите второе число: ");
+              int n2 = scan.nextInt();
+              int SS = Calculator.div(n1, n2); 
+              System.out.printf("%d / %d = %d", n1, n2, SS);
+            } catch (Exception error) {
+                System.out.println(error.getMessage());
+            }
+            }
+            else if ( menu == 5) {
+              System.out.print("Введите степень факториала: ");
+              int num = scan.nextInt();
+              long res = Calculator.fact(num);
+              System.out.printf("%d! = %d", num, res);
+            }
+            else if (menu == 6) {
+                System.out.print("Введите число: ");
+                double num = scan.nextDouble();
+                double res = Calculator.sqrt(num);
+                System.out.printf("sqrt(%f) = %f", num, res);
+            }
+            else if (menu ==7) {
             System.out.print("Введите число: ");
             double num = scan.nextDouble();
-            double res = Calculator.sqrt(num);
-            System.out.printf("sqrt(%f) = %f", num, res);
+            System.out.print("Введите степень: ");
+            int pow = scan.nextInt();
+            double res =Calculator.pow(num, pow);
+            System.out.printf("%f^%d = %f", num, pow, res);
+            }
+            else {
+                System.out.println("Вы ввели не корректное значение меню.");
+            }
         }
-        else if (menu ==7) {
-        System.out.print("Введите число: ");
-        double num = scan.nextDouble();
-        System.out.print("Введите степень: ");
-        int pow = scan.nextInt();
-        double res =Calculator.pow(num, pow);
-        System.out.printf("%f^%d = %f", num, pow, res);
-        }
-        else {
-            System.out.println("Вы ввели не корректное значение меню.");
-        }
-    }
-}
+   } 
+} 
 
  class Calculator {
     public static int sum(int nam1, int nam2) {
